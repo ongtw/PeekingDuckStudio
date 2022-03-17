31c257e86f6d8f0573260ad6f7f71a3942764aee
dotw <davidong@aisingapore.org>
2022-03-17 14:19:06 +0800

    add pipeline configuration parsing (alpha version)

--- config_parser.py
+++ config_parser.py
@@ -13,14 +13,15 @@ class ConfigParser:
         self.pkd_path = get_peekingduck_path()
         self.config_path = self.pkd_path / "configs"
         self.find_config_files()
-        self.parse_configs()
+        self.parse_pkd_configs()
 
     def find_config_files(self):
         files = self.config_path.glob("*")
         self.configs = sorted([file for file in files if file.is_dir()])
         # print("configs:", self.configs)
 
-    def parse_configs(self):
+    def parse_pkd_configs(self):
+        """Parse PeekingDuck node default configuration"""
         self.nodes_list = []
         self.nodes_by_type = dict()
         self.title_config_map = dict()
@@ -28,7 +29,7 @@ class ConfigParser:
 
         for config in self.configs:
             node_type = config.name
-            # print(node_type)
+            print(node_type)
             nodelist = []
 
             name_config_map = dict()
@@ -36,7 +37,7 @@ class ConfigParser:
 
             for config_file in files:
                 node_name = config_file.name[:-4]
-                # print(node_name)
+                print("  ", node_name)
                 with open(config_file) as file:
                     node_config = yaml.safe_load(file)
                 # print(node_config)
@@ -49,6 +50,35 @@ class ConfigParser:
             self.pkd_config[node_type] = name_config_map
             self.nodes_by_type[node_type] = nodelist
             self.nodes_list.extend(nodelist)
+
+    def verify_config(self, idx_to_node):
+        pipeline_errors = []
+
+        if idx_to_node:
+            data_types_available = set()
+            for i, nodeWidget in enumerate(idx_to_node):
+                node_title = nodeWidget.node_text
+                print(f"verifying {node_title}...")
+                if node_title.startswith("custom_nodes."):
+                    print("skipping custom node")
+                    continue
+                node_config = self.title_config_map[node_title]
+                node_input = node_config["input"]
+                node_output = node_config["output"]
+                # check input
+                for the_input in node_input:
+                    if the_input not in ["all", "none"]:
+                        if the_input not in data_types_available:
+                            print(f"data_types: {data_types_available}")
+                            print(f"'{the_input}' not available")
+                            pipeline_errors.append(i)
+                            break  # just need one error
+                # check output
+                for the_output in node_output:
+                    if the_output != "none":
+                        data_types_available.add(the_output)
+
+        return pipeline_errors
 
     def debug_configs(self):
         for node_type in self.pkd_config:
