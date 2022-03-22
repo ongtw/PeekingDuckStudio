7485ed5789f1e03e1c69014a32cd723079c7f2e0
dotw <davidong@aisingapore.org>
2022-03-23 00:23:41 +0800

    reorganise code for all widget classes to pkdgui_widgets.py

--- pkdgui.py
+++ pkdgui.py
@@ -63,7 +63,7 @@ from typing import Dict, List
 import os
 import yaml
 from config_parser import NodeConfigParser
-
+from pkdgui_widgets import Config, FileLoadDialog, Node, ScreenPipeline, ScreenPlayback
 
 # Todo:
 # - add pipeline node
@@ -75,81 +75,6 @@ from config_parser import NodeConfigParser
 # - support custom nodes definition
 # - app config: default folder, etc.
 # - PKD playback
-
-
-#
-# Kivy widget classes
-#
-class Config(GridLayout):
-    bkgd_color = ListProperty([0, 0, 1, 1])
-    select_color = ListProperty([0, 0, 0, 0])
-    config_key = ObjectProperty("key")
-    config_value = ObjectProperty("value")
-    config_set = BooleanProperty(False)
-    callback_double_tap = ObjectProperty(None)
-
-    def on_touch_down(self, touch):
-        if touch.is_double_tap:
-            if self.collide_point(*touch.pos):
-                print(f"double-click: Config {self.config_key} {self.pos} {self.size}")
-                if self.callback_double_tap:
-                    self.callback_double_tap(self)
-                return True  # stop event from percolating
-        return super().on_touch_down(touch)
-
-
-class FileLoadDialog(FloatLayout):
-    select = ObjectProperty(None)  # map to file/path selected method
-    cancel = ObjectProperty(None)  # map to cancel method
-
-    def setup(self, path: str, filters: List[str]):
-        # dotw: weakref, cannot use python with statement
-        self.ids["id_file_chooser"].rootpath = ROOT_PATH
-        self.ids["id_file_chooser"].path = path
-        self.ids["id_file_chooser"].filters = filters
-
-
-class Node(GridLayout):
-    bkgd_color = ListProperty([0, 0, 1, 1])
-    select_color = ListProperty([0, 0, 0, 0])
-    node_number = ObjectProperty("0")
-    node_text = ObjectProperty("")
-
-    def on_touch_down(self, touch):
-        if touch.is_double_tap:
-            if self.collide_point(*touch.pos):
-                print(f"Node {self.node_text}: double-click")
-                print(touch)
-                return True  # stop event from percolating
-        return super().on_touch_down(touch)
-
-
-class Output(GridLayout):
-    def on_touch_down(self, touch):
-        # if self.collide_point(*touch.pos):
-        #     print("Output: touch inside me")
-        self.touch_down_callback(touch)
-
-
-class ProjectInfo(GridLayout):
-    directory = StringProperty("")
-    filename = StringProperty("")
-
-
-class RoundedButton(BoxLayout):
-    color_bkgd = ListProperty([0, 0, 0, 0])
-    color_normal = ListProperty([65 / 255, 105 / 255, 225 / 255, 1])
-    color_pressed = ListProperty([30 / 255, 144 / 255, 1, 1])
-    color_separator = ListProperty([0, 0, 0, 1])
-    color_text = ListProperty([1, 1, 1, 1])
-
-
-class ScreenPipeline(Screen):
-    pass
-
-
-class ScreenPlayback(Screen):
-    pass
 
 
 ###
@@ -398,7 +323,7 @@ class pkdguiApp(App):
 
     def btn_load_file(self, btn, *args) -> None:
         file_dialog = FileLoadDialog(select=self.load_file, cancel=self.cancel_load)
-        file_dialog.setup(path=CURR_PATH, filters=FILE_FILTERS)
+        file_dialog.setup(root_path=ROOT_PATH, path=CURR_PATH, filters=FILE_FILTERS)
         self._file_dialog = Popup(
             title="Load File", content=file_dialog, size_hint=(0.75, 0.75)
         )
---
+++ pkdgui_widgets.py
@@ -0,0 +1,89 @@
+#
+# Kivy Widget Classes
+#
+from kivy.properties import (
+    BooleanProperty,
+    ListProperty,
+    ObjectProperty,
+    StringProperty,
+)
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import Screen
+
+from typing import List
+
+
+class Config(GridLayout):
+    bkgd_color = ListProperty([0, 0, 1, 1])
+    select_color = ListProperty([0, 0, 0, 0])
+    config_key = ObjectProperty("key")
+    config_value = ObjectProperty("value")
+    config_set = BooleanProperty(False)
+    callback_double_tap = ObjectProperty(None)
+
+    def on_touch_down(self, touch):
+        if touch.is_double_tap:
+            if self.collide_point(*touch.pos):
+                print(f"double-click: Config {self.config_key} {self.pos} {self.size}")
+                if self.callback_double_tap:
+                    self.callback_double_tap(self)
+                return True  # stop event from percolating
+        return super().on_touch_down(touch)
+
+
+class FileLoadDialog(FloatLayout):
+    select = ObjectProperty(None)  # map to file/path selected method
+    cancel = ObjectProperty(None)  # map to cancel method
+
+    def setup(self, root_path: str, path: str, filters: List[str]):
+        # dotw: weakref, cannot use python with statement
+        file_chooser = self.ids["id_file_chooser"]
+        file_chooser.rootpath = root_path
+        file_chooser.path = path
+        file_chooser.filters = filters
+
+
+class Node(GridLayout):
+    bkgd_color = ListProperty([0, 0, 1, 1])
+    select_color = ListProperty([0, 0, 0, 0])
+    node_number = ObjectProperty("0")
+    node_text = ObjectProperty("")
+
+    def on_touch_down(self, touch):
+        if touch.is_double_tap:
+            if self.collide_point(*touch.pos):
+                print(f"Node {self.node_text}: double-click")
+                print(touch)
+                return True  # stop event from percolating
+        return super().on_touch_down(touch)
+
+
+class Output(GridLayout):
+    def on_touch_down(self, touch):
+        # if self.collide_point(*touch.pos):
+        #     print("Output: touch inside me")
+        # self.touch_down_callback(touch)
+        pass
+
+
+class ProjectInfo(GridLayout):
+    directory = StringProperty("")
+    filename = StringProperty("")
+
+
+class RoundedButton(BoxLayout):
+    color_bkgd = ListProperty([0, 0, 0, 0])
+    color_normal = ListProperty([65 / 255, 105 / 255, 225 / 255, 1])
+    color_pressed = ListProperty([30 / 255, 144 / 255, 1, 1])
+    color_separator = ListProperty([0, 0, 0, 1])
+    color_text = ListProperty([1, 1, 1, 1])
+
+
+class ScreenPipeline(Screen):
+    pass
+
+
+class ScreenPlayback(Screen):
+    pass
